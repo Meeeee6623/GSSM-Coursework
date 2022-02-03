f5f82e33f149f99d34cc0079006ee2dde32b8bb4
Benjamin Chauhan <63178260+Meeeee6623@users.noreply.github.com>
2022-02-03 20:51:58 +0000

    ai work

--- Computational Physics/Unit 6/6.2.py
+++ Computational Physics/Unit 6/6.2.py
@@ -1,5 +1,5 @@
 import numpy as np
-from numpy import array
+from numpy import array, empty
 
 A = array([[2, 1, 4, 1],
            [3, 4, -1, -1],
@@ -9,14 +9,30 @@ v = array([-4, 3, 9, 7], float)
 N = len(v)
 
 for m in range(N):
-    max_ind = np.argmax(A[m:, m], axis=0)  # get max row below current
-    A[[m, max_ind], :] = A[[max_ind, m], :]  # swap rows (pivot)
-    v[m], v[max_ind] = v[max_ind], v[m]  # swap vector
+    max_ind = np.argmax(A[:, m])  # get index of max row for current column
+    if max_ind > m:
+      A[[m, max_ind], :] = A[[max_ind, m], :]  # swap rows (pivot)
+      v[m], v[max_ind] = v[max_ind], v[m]  # swap vector
+      print(f'swapped {m}, {max_ind}')
 
-    A[m, :] /= A[m, m]
+    print(A)
+
+    A[m, m:] /= A[m, m]
     v[m] /= A[m, m]
+    print(A)
 
     if m + 1 < N:
-        A[m+1:, :] -= A[m + 1:, m, np.newaxis] * np.concatenate(([[A[m, :]]] * 3))  # subtract scaled value from lower rows
+        A[m+1:, m:] -= A[m + 1:, m, np.newaxis] * np.concatenate(([[A[m, m:]]] * (N - m - 1)))  # subtract scaled value from lower rows
     print(A)
-    exit()
+    print()
+
+    # back substitute
+    x = empty(N)
+    
+
+    '''x = empty(N,float)
+      for m in range(N-1,-1,-1):
+        x[m] = v[m]
+        for i in range(m+1,N):
+        x [m] -= A [m, i] *X [i]
+        print(x) 
