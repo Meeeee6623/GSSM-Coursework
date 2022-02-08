ce03a631bc1df802c989f73b6ec12d343f0684db
Benjamin Chauhan <63178260+Meeeee6623@users.noreply.github.com>
2022-02-08 20:58:46 +0000

    fix?

--- Artificial Intelligence/Iris Training/train.py
+++ Artificial Intelligence/Iris Training/train.py
@@ -1,11 +1,30 @@
 import numpy as np
 
+def test(distances: list, euclid_data: np.ndarray):
+  test_data = np.concatenate(euclid_data[40:50], euclid_data[90:100], euclid_data[140:])
+  print(test_data)
+  setosa_dist = np.fabs(test_data - distances[0])
+  versicolor_dist = np.fabs(test_data - distances[0])
+  virginica_dist = np.fabs(test_data - distances[0])
+
+  correct = np.sum(
+    np.count_nonzero(test_data[0:10] -  
+    setosa_dist[0:10] < np.min(versicolor_dist[0:10], virginica_dist[0:10])),
+    np.count_nonzero(test_data[10:20] -  
+    versicolor_dist[10:20] < np.min(setosa_dist[10:20], virginica_dist[10:20])),
+    np.count_nonzero(test_data[20:] -  
+    setosa_dist[20:] < np.min(versicolor_dist[20:], virginica_dist[20:]))
+    )
+  wrong = len(test_data) - correct
+  return correct, wrong
+
 
 def calc_euclidian(data: np.ndarray):
-    s = np.average(np.sqrt(np.power(data[:40, :], 2)))
-    ve = np.average(np.sqrt(np.power(data[50:90, :], 2)))
-    vi = np.average(np.sqrt(np.power(data[100:140, :], 2)))
-    return s, ve, vi
+    euclid_data = np.sqrt(np.sum(np.power(data, 2), axis=1))
+    s = euclid_data[0:40]
+    ve = euclid_data[50:90]
+    vi = euclid_data[100:140]
+    return s, ve, vi, euclid_data
 
 
 iris_data = np.empty([150, 4])
@@ -16,6 +35,7 @@ with open('data.txt', 'r') as f:
     for index in np.arange(len(lines)):
         iris_data[index] = lines[index].split(',')[:4]
 
-print(iris_data)
-setosa, versicolor, virginica = calc_euclidian(iris_data)
-print(setosa, versicolor, virginica)
+# print(iris_data)
+setosa, versicolor, virginica, euclid_data = calc_euclidian(iris_data)
+print(euclid_data)
+print(test([setosa, versicolor, virginica], euclid_data))
