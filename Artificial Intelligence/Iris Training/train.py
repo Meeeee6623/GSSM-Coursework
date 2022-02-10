455e8e072c162a545cf6189ebe26b6d694deadf2
Meeeee6623 <c.benjaminsemail1@gmail.com>
2022-02-10 15:42:42 -0500

    add other distance metrics, re-format

--- Artificial Intelligence/Iris Training/train.py
+++ Artificial Intelligence/Iris Training/train.py
@@ -1,43 +1,32 @@
 import numpy as np
 
 
-def test_training(averages: list, iris_data: np.ndarray):
-    # grab all test values
-    test_data = np.concatenate([euclid_data[40:50], euclid_data[90:100], euclid_data[140:]])
-    # get distances to each type, for each testing value
-    setosa_dist = np.fabs(test_data - averages[0])
-    versicolor_dist = np.fabs(test_data - averages[1])
-    virginica_dist = np.fabs(test_data - averages[2])
-
+def classify(distances: list):
+    setosa_dist = distances[0]
+    versicolor_dist = distances[1]
+    virginica_dist = distances[2]
     # boolean array of correct vs. false classifications
     classifications = np.concatenate([setosa_dist[0:10] < np.minimum(versicolor_dist[0:10], virginica_dist[0:10]),
                                       versicolor_dist[10:20] < np.minimum(setosa_dist[10:20], virginica_dist[10:20]),
                                       virginica_dist[20:] < np.minimum(versicolor_dist[20:], setosa_dist[20:])])
 
     correct = np.count_nonzero(classifications)
-    wrong = len(test_data) - correct
+    wrong = len(setosa_dist) - correct
     wrong_indexes = np.where(~classifications)[0]
-    # indexes of falsely classified values in original euclid_data array
-    wrong_indexes = np.concatenate([np.where(wrong_indexes < 10)[0] + 40,
-                                    np.where((10 < wrong_indexes) & (wrong_indexes < 20))[0] + 80,
-                                    np.where(20 < wrong_indexes)[0] + 120])
+    print(f'Accuracy: {np.round(correct / len(setosa_dist), 3)}%')
     print(f'{correct} Irises were correctly classified.')
     print(f'{wrong} Irises were incorrectly classified:')
     for i in wrong_indexes:
         iris_types = ['setosa', 'versicolor', 'virginica']
-        nearest_dist = np.round(np.amin(np.fabs(euclid_data[i] - distances)), 3)
-        nearest_index = np.argmin(np.fabs(euclid_data[i] - distances)) - 1
+        nearest_dist = np.round(np.amin(np.array([setosa_dist[i], versicolor_dist[i], virginica_dist[i]])), 3)
+        nearest_index = np.argmin(np.array([setosa_dist[i], versicolor_dist[i], virginica_dist[i]]))
         print(
-            f'A {iris_types[i // 40 - 1]} was incorrectly classified as a {iris_types[nearest_index]} (distance of {nearest_dist})')
+            f'A {iris_types[i // 10]} was incorrectly classified as a {iris_types[nearest_index]} (distance of {nearest_dist})')
+    # indexes of falsely classified values in original iris_data array
+    wrong_indexes = np.concatenate([np.where(wrong_indexes < 10)[0] + 40,
+                                    np.where((10 < wrong_indexes) & (wrong_indexes < 20))[0] + 80,
+                                    np.where(20 < wrong_indexes)[0] + 120])
     return correct, wrong, wrong_indexes
-
-
-def calc_euclidian(data: np.ndarray):
-    # mean per flower type
-    s = np.mean(data[0:40])
-    ve = np.mean(data[50:90])
-    vi = np.mean(data[100:140])
-    return np.array([s, ve, vi])
 
 
 iris_data = np.empty([150, 4])
@@ -47,5 +36,47 @@ with open('data.txt', 'r') as f:
     for index in np.arange(len(lines)):
         iris_data[index] = lines[index].split(',')[:4]
 
-averages = calc_euclidian(iris_data)
-test_training(distances, iris_data)
+# get all test data
+test_data = np.concatenate([iris_data[40:50], iris_data[90:100], iris_data[140:]])
+
+# average values per class
+averages = np.array([np.mean(iris_data[0:40], axis=0),
+                    np.mean(iris_data[50:90], axis=0),
+                    np.mean(iris_data[100:140], axis=0)])
+
+
+def calc_euclidian_distances(averages, test_data):
+    # euclidian distances for each test case
+    setosa_dist = np.sqrt(np.sum(np.power(test_data - averages[0], 2), axis=1))
+    versicolor_dist = np.sqrt(np.sum(np.power(test_data - averages[1], 2), axis=1))
+    virginica_dist = np.sqrt(np.sum(np.power(test_data - averages[2], 2), axis=1))
+    return [setosa_dist, versicolor_dist, virginica_dist]
+
+
+print("Euclidian Distance Results: ")
+euclidian_distances = calc_euclidian_distances(averages, test_data)
+classify(euclidian_distances)
+
+
+def calc_manhattan_distances(averages, test_data):
+    setosa_dist = np.sum(np.fabs(test_data - averages[0]), axis=1)
+    versicolor_dist = np.sum(np.fabs(test_data - averages[1]), axis=1)
+    virginica_dist = np.sum(np.fabs(test_data - averages[2]), axis=1)
+    return [setosa_dist, versicolor_dist, virginica_dist]
+
+
+print("Manhattan Distance Results: ")
+manhattan_distances = calc_manhattan_distances(averages, test_data)
+classify(manhattan_distances)
+
+
+def calc_chebyshev_distances(averages, test_data):
+    setosa_dist = np.max(np.fabs(test_data - averages[0]), axis=1)
+    versicolor_dist = np.max(np.fabs(test_data - averages[1]), axis=1)
+    virginica_dist = np.max(np.fabs(test_data - averages[2]), axis=1)
+    return [setosa_dist, versicolor_dist, virginica_dist]
+
+
+print("Chebyshev Distance Results: ")
+chebyshev_distances = calc_chebyshev_distances(averages, test_data)
+classify(chebyshev_distances)
