06ead1d64fa63baaa03a2ed92c2fe58491cfbe01
Meeeee6623 <c.benjaminsemail1@gmail.com>
2022-04-12 15:58:16 -0400

    fix genetic

--- Artificial Intelligence/ga.py
+++ Artificial Intelligence/ga.py
@@ -2,13 +2,21 @@ from random import choice
 
 import numpy as np
 
+NUM_GENOMES = 40
+POP_SIZE = 100
+
+OPTIMAL = np.asarray([[1] * NUM_GENOMES][0])
+
 
 def fitness(pop):
-    return np.abs(pop - np.asarray([0, 1, 0, 1, 0]))
+    return np.sum(pop == OPTIMAL, axis=1)
 
 
 def generate_population(n):
-    return np.random.random(n) > 0.5
+    pop = np.zeros((n, NUM_GENOMES))
+    mask = np.random.rand(n, NUM_GENOMES) < 0.5
+    pop[mask] = 0
+    return pop
 
 
 def next_gen(pop, elite):
@@ -16,7 +24,7 @@ def next_gen(pop, elite):
     np.random.shuffle(pop)
     elite = [elite[0], elite[1], elite[2]]
     for i, member in enumerate(pop[:-2]):
-        if i < pop.size // 10:
+        if i < pop.size // 20:
             new_gen[i] = mutate(member)
         else:
             new_gen[i] = crossover(choice(elite), member)
@@ -28,7 +36,7 @@ def next_gen(pop, elite):
 
 
 def mutate(p):
-    r = np.random.randint(0, p.size)
+    r = np.random.randint(0, NUM_GENOMES)
     if p[r] == 0:
         p[r] = 1
     else:
@@ -36,15 +44,22 @@ def mutate(p):
     return p
 
 
-def crossover(p1, p2):
-    return np.concatenate(p1[0:p1.size // 2], p2[p1.size // 2:])
+def crossover(elite, p2):
+    r = np.random.randint(0, NUM_GENOMES)
+    return np.concatenate([elite[0:r], p2[r:]])
 
 
-population = generate_population(100)
-scores = fitness(population)
-top_3 = population[np.argpartition(scores, 3)]
-print(top_3)
-for i in range(100):
-    population = next_gen(population, top_3)
+convergence = []
+for gen in range(5):
+    population = generate_population(POP_SIZE)
     scores = fitness(population)
-    top_3 = population[np.argpartition(scores, 3)]
+    top_3 = population[np.argpartition(scores, -3)[-3:]]
+    i = 2
+    while ~np.any(scores == 20):
+        population = next_gen(population, top_3)
+        scores = fitness(population)
+        top_3 = population[np.argpartition(scores, -3)[-3:]]
+        i += 1
+    print(f'Converged at generation {i}')
+    convergence.append(i)
+print(f'Average Convergence: {np.mean(convergence)}')
